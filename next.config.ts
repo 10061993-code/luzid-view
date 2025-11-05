35347d81c94ede2542445213f1aedc8051162566
Christoph Maximilian Gaidies <christophmaximiliangaidies@Christophs-MacBook-Air.local>
2025-11-05 13:59:07 +0100

    chore: prepare for eslint flat-config codemod

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
+  // Next 15: typedRoutes nicht mehr unter "experimental"
+  typedRoutes: true,
+  // Verhindert, dass ein ESLint-Patch-Problem den Build stoppt
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [],
   },
-  images: { remotePatterns: [] },
   webpack(config) {
     return config;
   },
 };
 
 export default nextConfig;
+
